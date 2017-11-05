4f407bb41e33676789a6113c9977ca18b4bbaf59
Marcel DellWin <git@b-velop.com>
2017-11-05 01:07:25 +0100

    Observer

--- KohonenMap/MainWindow.xaml.cs
+++ KohonenMap/MainWindow.xaml.cs
@@ -1,21 +1,34 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using com_b_velop.Common;
+using com_b_velop.Enums;
 
 namespace com_b_velop
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IObserver
     {
         public MainWindow()
         {
             InitializeComponent();
+            var app = AppState.GetInstance();
+            app.Register(this);
         }
 
         private void MainWindow_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             this.Title += '*';
+        }
+
+        public void Update(ObserverType obj)
+        {
+            if (obj == ObserverType.TrainDataInfo)
+            {
+                var info = SomModel.GetTrainDataInfo();
+                Title = info.Features + " Spalten - " + info.TrainDataSize + " Zeilen";
+            }
         }
     }
 }
